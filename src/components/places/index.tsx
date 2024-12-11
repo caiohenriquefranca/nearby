aa80e495f83fabb1461dd613f7da150100585273
Caio Henrique <caiohenriquefrancav@icloud.com>
2024-12-11 19:03:21 -0300

    Inserir mapa e att

--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -1,40 +1,45 @@
 import { Text, useWindowDimensions } from "react-native";
-import { Place, PlaceProps } from "../place"
+import { Place, PlaceProps } from "../place";
 import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
 import { useRef } from "react";
 import { s } from "./styles";
+import { router } from "expo-router";
 
 type Props = {
-    data: PlaceProps[]
-}
+  data: PlaceProps[];
+};
 
+export function Places({ data }: Props) {
+  const dimension = useWindowDimensions();
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = {
+    min: 278,
+    max: dimension.height - 128,
+  };
 
-export function Places({ data }: Props){
-    const dimension = useWindowDimensions()
-    const bottomSheetRef = useRef<BottomSheet>(null)
-    const snapPoints = {
-        min: 278,
-        max: dimension.height - 128
-    }
-    
-    return (
-        <BottomSheet 
-        ref={bottomSheetRef}
-        snapPoints={[snapPoints.min,snapPoints.max]}
-        handleIndicatorStyle={s.indicator}
-        backgroundStyle={s.container}
-        enableOverDrag={false}
-        >
-        <BottomSheetFlatList
-            data={data}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => <Place data={item}/>}
-            contentContainerStyle={s.content}
-            ListHeaderComponent={() => (
-                <Text style={s.title}>Explore locais perto de você</Text>
-            )}
-            showsVerticalScrollIndicator={false}        
-        />
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      snapPoints={[snapPoints.min, snapPoints.max]}
+      handleIndicatorStyle={s.indicator}
+      backgroundStyle={s.container}
+      enableOverDrag={false}
+    >
+      <BottomSheetFlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Place
+            data={item}
+            onPress={() => router.navigate(`/market/${item.id}`)}
+          />
+        )}
+        contentContainerStyle={s.content}
+        ListHeaderComponent={() => (
+          <Text style={s.title}>Explore locais perto de você</Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
     </BottomSheet>
-    )
+  );
 }
